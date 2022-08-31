53e39794cbc10db169398847b8b4757dc2a39c41
Shashwot Pradhan <shashwotpradhan@gmail.com>
2022-08-31 08:17:21 +0545

    practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Netflix from './Netflix';
 
+
+import Amazon from './Amazon';
+ const favSeries = 'amazon';
+
+ const FavS = () => {
+ if (favSeries === 'netflix'){
+  return <Netflix />;
+ }
+ else{
+  return <Amazon />;
+ }
+};
+const App = () => (
+  <>
+  <h1>List of movies </h1>
+ <FavS />
+   
+  </>
+)
 export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata = [
+{
+    id: 1,
+    sname:"hi",
+    imgsrc:"https://cdnportal.mobalytics.gg/production/2022/05/06ee0025-genshin-impact-eula-feature.png",
+    title:"Eula",
+    link:"",
+},
+{
+    id: 2,
+    sname:"bye",
+    imgsrc:"",
+    title:"Hu tao",
+    link:"",
+},
+{
+    id: 3,
+    sname:"htr",
+    imgsrc:"https://cdn.oneesports.gg/cdn-data/2021/07/Genshin_Impact_Ayaka_Materials_Banner.jpg",
+    title:"Ayaka",
+    link:"",
+},
+{
+    id: 4,
+    sname:"name",
+    imgsrc:"https://cdn.oneesports.gg/cdn-data/2021/07/Genshin_Impact_Ayaka_Materials_Banner.jpg",
+    title:"Kazuha",
+    link:"",
+},
+{
+    id: 5,
+    sname:"nam",
+    imgsrc:"",
+    title:"Ganyu",
+    link:"",
+},
+
+];
+export default Sdata;
